9bf6548098894bda1b46e4382b1dd6684c313d5c
Talmaj Marinc <talmaj@gmail.com>
2014-12-03 14:31:13 +0100

    Fixed the fbCrawl regex

--- scripts/crawlFB.py
+++ scripts/crawlFB.py
@@ -4,6 +4,7 @@ import csv
 import re
 import datetime
 import sys
+import traceback
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,13 +26,19 @@ csvfile = open(path, 'rb')
 linkreader = csv.reader(csvfile)
 linkreader.next()
 for row in linkreader:
-    print(row)
+    #print(row)
     location_string = re.sub('[\[\]<>]','',row[1]).split(',')
     for location in location_string:
-        if "/" in location:
-            locations.append(location.split('/')[1].strip())
+        if 'pages/' in location or 'gid=' in location:
+            try:
+                location = re.findall('[/=](\d+)[/\?]?', location)[0]
+            except:
+                print 'WARNING', location, 'not found'
+                continue
         else:
-            locations.append(location.strip())
+            location = location.strip()
+        print location
+        locations.append(location)
 
 #Always replace with a fresh key
 graph = facebook.GraphAPI(api_key)
@@ -43,18 +50,32 @@ for location in locations:
     #TODO: create location model save and relate id to event models
     #check if location already exists by saving fb_id
         events = graph.get_connections(location_fb['id'], 'events')
-        print bcolors.OKGREEN + location_fb['name'] + bcolors.ENDC
+        print bcolors.OKGREEN + location_fb['name'].encode('ascii', 'replace') + bcolors.ENDC
 
         for event in events['data']:
             #check if event already exists by saving also fb_id
             date = datetime.datetime.strptime('2011-03-06T03:36:45+0000','%Y-%m-%dT%H:%M:%S+0000')
-            print '-----' + event['name']
+            print '-----' + event['name'].encode('ascii', 'replace') # only for the terminal
+            
+            ## WARNING UnicodeEncodeError without .encode('ascii', 'replace') - maybe use: 
+            ## import sys
+            ## from kitchen.text.converters import getwriter
+            ## UTF8Writer = getwriter('utf8')
+            ## sys.stdout = UTF8Writer(sys.stdout)
+
             #get description
-            event_detail = graph.get_object(event['id'])['description']
+            try:
+                event_detail = graph.get_object(event['id'])['description']
+            except KeyError, error:
+                print bcolors.WARNING + 'KeyError: {0} retrieving event description from {1}'.format(error, event['name']) + bcolors.ENDC
             #TODO: extract artists
             #TODO: create event model
-    except Exception, e:
-        print bcolors.WARNING + 'Error retrieving events from {}'.format(location) + bcolors.ENDC
+
+    except Exception, error:
+        print bcolors.WARNING + '{0} retrieving events from {1}'.format(error, location) + bcolors.ENDC
+        if type(error) != facebook.GraphAPIError:
+            print u'Error: {}\n\nTraceback:\n{}'.format(error, traceback.format_exc())
+
         
 
 def cleanName(string):
